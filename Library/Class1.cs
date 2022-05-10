39910f34365dbdb8793cf23d57d200aa38e2786f
vidya2906 <101714184+vidya2906@users.noreply.github.com>
2022-05-11 01:42:47 +0530

    Add files via upload

---
+++ Library/Class1.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Class1
+    {
+        public IWebDriver Driver;
+        public IJavaScriptExecutor Js;
+        public void Reference(string url)
+        {
+            Driver = new ChromeDriver();
+            Driver.Navigate().GoToUrl(url);
+            Driver.Manage().Window.Maximize();
+        }
+        public void Wait(int t)
+        {
+            Thread.Sleep(t);
+        }
+
+        public void ScrollPage(int hz, int ver)
+        {
+            ((IJavaScriptExecutor)Driver).ExecuteScript("window.scrollBy(" + hz + ", " + ver + ")");
+        }
+
+        public IWebElement Findid(string id)
+        {
+
+            return Driver.FindElement(By.Id(id));
+
+        }
+
+
+        public IWebElement FindXpath(string XPath)
+        {
+            return Driver.FindElement(By.XPath(XPath));
+        }
+        public IWebElement Findxpaths(string XPaths)
+        {
+            return Driver.FindElements(By.XPath(XPaths))[0];
+        }
+        public void quit()
+        {
+            Task.Delay(120000).Wait();
+            Driver.Close();
+            Driver.Quit();
+        }
+    }
+}
