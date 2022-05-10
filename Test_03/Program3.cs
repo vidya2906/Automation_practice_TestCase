201c35c632ccd27f21cda8f5c663d9f67c58e2e0
vidya2906 <101714184+vidya2906@users.noreply.github.com>
2022-05-11 01:57:36 +0530

    Add files via upload

---
+++ Test_03/Program3.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library;
+using OpenQA.Selenium;
+
+//Test Case 3 : Can a registered user able to view all the products listed on the website?
+
+namespace Test_03
+{
+    internal class Program3 : Class1
+    {
+        static void Main(string[] args)
+        {
+            Program3 test = new Program3();
+            test.case3();
+        }
+
+        public void case3()
+        {
+            //Creating reference to browser
+            Reference("http://automationpractice.com/index.php");
+            IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
+
+            Wait(2000);
+
+            IWebElement womenBTN = FindXpath("//a[@title='Women']");
+            womenBTN.Click();
+            Wait(1500);
+
+            ScrollPage(0, 200);
+            Wait(1000);
+
+            ScrollPage(0, 200);
+            Wait(1000);
+
+            ScrollPage(0, 200);
+            Wait(1000);
+
+            ScrollPage(0, 200);
+            Wait(1000);
+
+            ScrollPage(0, 200);
+            Wait(1000);
+
+            ScrollPage(0, 200);
+            Wait(1000);
+
+            ScrollPage(0, 200);
+            Wait(1000);
+
+            ScrollPage(0, 200);
+            Wait(1000);
+
+            ScrollPage(0, 200);
+            Wait(1000);
+
+            quit();
+        }
+    }
+}
